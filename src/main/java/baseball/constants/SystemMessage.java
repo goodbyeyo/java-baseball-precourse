dc72f21570dafdc417607926b7445144e47009a8
Sangwook Hwang <yoonseol1983@gmail.com>
2022-10-03 22:17:15 +0900

    feat(view) : 스트라이크, 볼, 낫싱 결과 출력 메세지 구현

--- src/main/java/baseball/constants/SystemMessage.java
+++ src/main/java/baseball/constants/SystemMessage.java
@@ -1,7 +1,10 @@
 package baseball.constants;
 
 public enum SystemMessage {
-    INPUT_WAITING("숫자를 입력해주세요 : ");
+    INPUT_WAITING("숫자를 입력해주세요 : "),
+    JUDGE_STRIKE("스트라이크"),
+    JUDGE_BALL("볼"),
+    JUDGE_NOTHING("낫싱");
 
     private final String message;
 
--- src/main/java/baseball/view/ResultView.java
+++ src/main/java/baseball/view/ResultView.java
@@ -1,9 +1,30 @@
 package baseball.view;
 
+import baseball.constants.SystemMessage;
+
+import static baseball.constants.SystemMessage.*;
+
 public class ResultView {
 
     // TODO 스트라이크, 볼, 낫싱 일 경우 각각 message 리턴 구현
     public static String getResultViewMessage(int strikes, int balls) {
-        return null;
+        StringBuilder message = new StringBuilder();
+        if (strikes == 0 && balls == 0) {
+            return JUDGE_NOTHING.getMessage();
+        }
+
+        if (balls != 0) {
+            message.append(balls).append(JUDGE_BALL.getMessage());
+        }
+
+        if (strikes != 0) {
+            message.append(strikes).append(JUDGE_STRIKE.getMessage());
+        }
+
+        if (strikes != 0 && balls != 0) {
+            message.append(" ");
+        }
+
+        return message.toString();
     }
 }
